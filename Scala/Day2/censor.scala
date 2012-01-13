62a54730da4d99f510dcdda2a00d277a17ff0af3
Tobias Bayer <tobayer@googlemail.com>
2012-01-13 13:13:35 +0100

    read words from file

--- Scala/Day2/censor.scala
+++ Scala/Day2/censor.scala
@@ -1,6 +1,6 @@
 trait Censor {
 
-  val toCensor = Map("Shoot" -> "Pucky", "Darn" -> "Beans")
+  val toCensor = readCensorMap
   
   def censor(message: String) = {
     val words = message.split(" ")
@@ -8,6 +8,18 @@ trait Censor {
     val censored = words.map(word => toCensor.getOrElse(word, word))
     // something like list.join() using foldLeft
     censored.foldLeft("")((joined, elem) => joined + (if(joined == "") "" else " ") + elem)
+  }
+
+  def readCensorMap = {
+    var map = Map[String, String]()
+    scala.io.Source.fromFile("subst.txt", "UTF8").getLines.foreach {
+      line => 
+      val key = line.split(" ")(0)
+      val value = line.split(" ")(1)
+      map += (key -> value)
+    }
+
+    map
   }
 }
 
